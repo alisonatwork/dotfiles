d5b48bce9b3d922484a62100a159aed5a28e4fd4
Alison Winters <alisonatwork@outlook.com>
2021-01-25 19:35:51 -0800

    use powershell to restart the service instead of net command

--- scripts/Update-MyBase.ps1
+++ scripts/Update-MyBase.ps1
@@ -4,12 +4,14 @@ $DnsCryptProxyDir = "$env:ChocolateyInstall\lib\dnscrypt-proxy\tools\win64"
 if (Test-Path($DnsCryptProxyDir)) {
   echo "Downloading latest mybase..."
   iwr https://download.dnscrypt.info/blacklists/domains/mybase.txt -OutFile "$env:TEMP\mybase.txt"
-  net stop dnscrypt-proxy
+  echo "Stopping dnscrypt-proxy..."
+  spsv dnscrypt-proxy
   echo "Replacing $DnsCryptProxyDir\mybase.txt"
   del "$DnsCryptProxyDir\mybase.txt.old"
   move "$DnsCryptProxyDir\mybase.txt" "$DnsCryptProxyDir\mybase.txt.old"
   move "$env:TEMP\mybase.txt" "$DnsCryptProxyDir\mybase.txt"
-  net start dnscrypt-proxy
+  echo "Starting dnscrypt-proxy..."
+  sasv dnscrypt-proxy
 } else {
   echo "Can't find dnscrypt-proxy at $DnsCryptProxyDir"
 }
