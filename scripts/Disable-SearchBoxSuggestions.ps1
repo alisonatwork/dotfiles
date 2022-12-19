4e6c7249c1faf459745e357fb923394e53af497b
Alison Winters <alisonatwork@outlook.com>
2022-12-19 10:22:23 +0800

    create path if it isn't already there

--- scripts/Disable-SearchBoxSuggestions.ps1
+++ scripts/Disable-SearchBoxSuggestions.ps1
@@ -2,4 +2,5 @@
 
 echo "Setting DisableSearchBoxSuggestions 1..."
 $path = 'HKCU:\Software\Policies\Microsoft\Windows\Explorer'
+ni $path -force | out-null
 sp -path $path -name 'DisableSearchBoxSuggestions' -value 1
