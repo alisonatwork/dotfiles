f094596247d65a7dd45623e67adde25821a32d37
Alison Winters <alisonatwork@outlook.com>
2023-07-06 22:36:30 +0800

    first take at a script to set up windows

---
+++ scripts/Bootstrap-Windows.ps1
@@ -0,0 +1,27 @@
+#!/usr/bin/env powershell
+
+# util
+winget install 7zip.7zip
+winget install Anki.Anki
+winget install Genymobile.scrcpy --scope machine
+winget install Git.Git
+winget install Google.EarthPro
+winget install Microsoft.VisualStudioCode --scope machine
+winget install Redhat.Podman
+winget install SumatraPDF.SumatraPDF
+
+# net
+winget install eloston.ungoogled-chromium
+winget install qBittorrent.qBittorrent
+winget install DNSCrypt.dnscrypt-proxy --scope machine
+winget install DominikReichl.KeePass
+winget install Mozilla.Thunderbird
+winget install OpenVPNTechnologies.OpenVPN
+winget install PrestonN.FreeTube
+
+# media
+winget install clsid2.mpc-hc
+winget install dotPDNLLC.paintdotnet
+winget install MusicBee.MusicBee
+winget install OpenMPT.OpenMPT
+winget install Valve.Steam
