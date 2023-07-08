921f83e8998d71f95bd7c50da83ae751064d11dd
Alison Winters <alisonatwork@outlook.com>
2023-07-08 13:50:23 +0800

    add script to set up explorer options

---
+++ scripts/Reset-ExplorerOptions.ps1
@@ -0,0 +1,14 @@
+#!/usr/bin/env powershell
+
+$path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+@{
+    DontPrettyPath               = 1
+    Hidden                       = 1
+    HideFileExt                  = 0
+    NavPaneExpandToCurrentFolder = 1
+    ShowSuperHidden              = 0
+}.GetEnumerator() | foreach {
+    echo "Setting $($_.key) $($_.value)..."
+    sp -path $path -name $_.key -value $_.value
+}
+kill -processname explorer
