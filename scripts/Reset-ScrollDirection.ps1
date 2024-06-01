8f2a3300032822376bdf53d35575ae7ff90f14c2
Alison Winters <alisonatwork@outlook.com>
2024-06-01 13:13:21 +0800

    use a looser glob to match bluetooth devices

--- scripts/Reset-ScrollDirection.ps1
+++ scripts/Reset-ScrollDirection.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env powershell
 
 echo "Setting FlipFlopWheel 1..."
-$glob = 'HKLM:\SYSTEM\CurrentControlSet\Enum\HID\VID*\*\Device Parameters'
+$glob = 'HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*VID*\*\Device Parameters'
 gp -path $glob -name 'FlipFlopWheel' -ea silent |
   foreach {
     echo ($_.pspath -replace '^.*::','')
