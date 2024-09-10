7b2273af9fd0f6b136ab55fb3cd250e51c40c614
Alison Winters <alisonatwork@outlook.com>
2024-09-10 13:13:10 +0800

    remove windows maps scripts

--- scripts/Set-MapsChina.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env powershell
-
-echo "Setting ChinaVariant 1..."
-$path = 'HKLM:\SYSTEM\Maps'
-sp -path $path -name 'ChinaVariant' -value 1
-sp -path $path -name 'OEMChinaVariantWin10' -value 1
-
-echo "Now delete all maps, Get-AppxPackage *Maps* | Remove-AppxPackage and reboot"
-
--- scripts/Set-MapsGlobal.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env powershell
-
-echo "Setting ChinaVariant 0..."
-$path = 'HKLM:\SYSTEM\Maps'
-sp -path $path -name 'ChinaVariant' -value 0
-sp -path $path -name 'OEMChinaVariantWin10' -value 0
-
-echo "Now delete all maps, Get-AppxPackage *Maps* | Remove-AppxPackage and reboot"
-
