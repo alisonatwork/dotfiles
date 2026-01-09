7af2368470de0f8de0bffbc917d54b215a184209
Alison Winters <alisonatwork@outlook.com>
2026-01-10 02:16:33 +0800

    only set login shell when SHELL is set

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -23,7 +23,9 @@ vim.filetype.add({
 
 vim.keymap.set('n', '`', '~')
 
-vim.o.shell = '"' .. vim.env.SHELL .. '" -l'
+if vim.env.SHELL then
+    vim.o.shell = '"' .. vim.env.SHELL .. '" -l'
+end
 if vim.fn.has('win32') == 1 then
     vim.keymap.set('n', '<C-z>', ':split<CR><C-w><C-w>:set nonu<CR>:terminal<CR>i')
 end
